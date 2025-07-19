09735726071bf255501afe159f759f5edb3488d2
Ganesha Moorthi. M <152759878+Ganeshmoorthii@users.noreply.github.com>
2025-07-19 08:11:02 +0530

    Update removeDuplicates.java

--- Arrays/Easy/removeDuplicates.java
+++ Arrays/Easy/removeDuplicates.java
@@ -1,3 +1,18 @@
+// 1. Using HashSet (Unsorted Array – Best for Interviews)
+public int[] removeDuplicates(int[] arr) {
+    Set<Integer> set = new LinkedHashSet<>(); // preserves order
+    for (int num : arr) {
+        set.add(num);
+    }
+    int[] result = new int[set.size()];
+    int i = 0;
+    for (int num : set) {
+        result[i++] = num;
+    }
+    return result;
+}
+
+// 2. In-place Removal for Sorted Array (like Leetcode #26)
 class Solution {
     public int removeDuplicates(int[] nums) {
         int i = 0;
@@ -9,4 +24,26 @@ class Solution {
         }
         return i+1;
     }
+}
+
+// 3. Using Java 8+ Streams (for unique result)
+public int[] removeDuplicates(int[] arr) {
+    return Arrays.stream(arr)
+                 .distinct()
+                 .toArray();
+}
+
+// 4. Brute-force (Avoid – For Learning)
+public int[] removeDuplicates(int[] arr) {
+    List<Integer> list = new ArrayList<>();
+    for (int i = 0; i < arr.length; i++) {
+        if (!list.contains(arr[i])) {
+            list.add(arr[i]);
+        }
+    }
+    int[] result = new int[list.size()];
+    for (int i = 0; i < result.length; i++) {
+        result[i] = list.get(i);
+    }
+    return result;
 }
