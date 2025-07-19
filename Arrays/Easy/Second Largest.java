e4de4a36108df686ca033ff39cff1852e2e04873
Ganesha Moorthi. M <152759878+Ganeshmoorthii@users.noreply.github.com>
2025-07-19 08:08:52 +0530

    Update Second Largest.java

--- Arrays/Easy/Second Largest.java
+++ Arrays/Easy/Second Largest.java
@@ -1,3 +1,35 @@
+// 1. Sorting Approach
+public int secondLargest(int[] arr) {
+    Arrays.sort(arr);
+    int max = arr[arr.length - 1];
+    for (int i = arr.length - 2; i >= 0; i--) {
+        if (arr[i] != max) {
+            return arr[i];
+        }
+    }
+    return -1; // No second largest exists
+}
+
+// 2. Two Pass Approach
+public int secondLargest(int[] arr) {
+    int max = Integer.MIN_VALUE;
+    for (int num : arr) {
+        if (num > max) {
+            max = num;
+        }
+    }
+
+    int second = Integer.MIN_VALUE;
+    for (int num : arr) {
+        if (num > second && num < max) {
+            second = num;
+        }
+    }
+
+    return second == Integer.MIN_VALUE ? -1 : second;
+}
+
+// 3. Optimal Single Pass Approach
 class Solution {
     public int print2largest(int[] arr) {
         int largest = arr[0], slargest = -1;
@@ -16,3 +48,26 @@ class Solution {
         return slargest;
     }
 }
+
+// 4. Using TreeSet (Only for Unique Elements)
+public int secondLargest(int[] arr) {
+    TreeSet<Integer> set = new TreeSet<>();
+    for (int num : arr) set.add(num);
+    
+    if (set.size() < 2) return -1;
+    
+    set.remove(set.last());
+    return set.last();
+}
+
+// 5. Streams Approach (Java 8+)
+public int secondLargest(int[] arr) {
+    return Arrays.stream(arr)
+        .distinct()
+        .boxed()
+        .sorted(Comparator.reverseOrder())
+        .skip(1)
+        .findFirst()
+        .orElse(-1);
+}
+
