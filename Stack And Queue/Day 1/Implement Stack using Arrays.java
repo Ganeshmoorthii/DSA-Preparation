e4aef7688416dbd2d2b994821b97688e2b6f789d
Ganesha Moorthi. M <152759878+Ganeshmoorthii@users.noreply.github.com>
2025-08-03 19:16:20 +0530

    Update Implement Stack using Arrays.java

--- Stack And Queue/Day 1/Implement Stack using Arrays.java
+++ Stack And Queue/Day 1/Implement Stack using Arrays.java
@@ -1,30 +1,4 @@
-/*Write a program to implement a Stack using Array. Your task is to use the class as shown in the comments in the code editor and complete the functions push() and pop() to implement a stack. The push() method takes one argument, an integer 'x' to be pushed into the stack and pop() which returns an integer present at the top and popped out from the stack. If the stack is empty then return -1 from the pop() method.
-
-Note: The input is given in form of queries. Since there are two operations push() and pop(), there is two types of queries as described below:
-(i) 1 x   (a query of this type means  pushing 'x' into the stack)
-(ii) 2     (a query of this type means to pop an element from the stack and print the popped element)
-Input contains separated by space and as described above. 
-
-Examples :
-
-Input: 1 2 1 3 2 1 4 2 
-Output: 3, 4
-Explanation: 
-push(2)    the stack will be {2}
-push(3)    the stack will be {2 3}
-pop()      poped element will be 3,
-           the stack will be {2}
-push(4)    the stack will be {2 4}
-pop()      poped element will be 4
-Input: 2 1 4 1 5 2
-Output: -1, 5
-Expected Time Complexity: O(1)
-Expected Space Complexity: O(1)
-
-Constraints:
-1 <= numbers of calls made to push, pop <= 100
-1 <= x <= 100*/
-
+// Write a program to implement a Stack using Array. 
 class MyStack {
     private int[] arr;
     private int top;
@@ -42,5 +16,118 @@ class MyStack {
         // Your Code
         if(top==-1) return -1;
         return arr[top--];
+    }
+}
+
+
+// Stack with Dynamic Resizing (like ArrayList)
+class DynamicStack {
+    private int[] stack;
+    private int top;
+
+    public DynamicStack() {
+        stack = new int[2]; // initial capacity
+        top = -1;
+    }
+
+    public void push(int x) {
+        if (top == stack.length - 1) {
+            resize(2 * stack.length); // double size
+        }
+        stack[++top] = x;
+    }
+
+    public int pop() {
+        if (isEmpty()) throw new RuntimeException("Stack Underflow");
+        int val = stack[top--];
+        if (top > 0 && top == stack.length / 4) {
+            resize(stack.length / 2); // shrink
+        }
+        return val;
+    }
+
+    public int peek() {
+        if (isEmpty()) throw new RuntimeException("Stack is empty");
+        return stack[top];
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    private void resize(int newSize) {
+        int[] newStack = new int[newSize];
+        for (int i = 0; i <= top; i++) {
+            newStack[i] = stack[i];
+        }
+        stack = newStack;
+    }
+}
+
+// Stack Using Linked List (Dynamic Memory Allocation)
+class LinkedStack {
+    private Node top;
+
+    private class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public void push(int x) {
+        Node newNode = new Node(x);
+        newNode.next = top;
+        top = newNode;
+    }
+
+    public int pop() {
+        if (isEmpty()) throw new RuntimeException("Stack Underflow");
+        int val = top.data;
+        top = top.next;
+        return val;
+    }
+
+    public int peek() {
+        if (isEmpty()) throw new RuntimeException("Stack is empty");
+        return top.data;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+}
+
+// Stack Using Generics (Works with any type)
+class GenericStack<T> {
+    private Object[] stack;
+    private int top;
+
+    public GenericStack(int size) {
+        stack = new Object[size];
+        top = -1;
+    }
+
+    public void push(T x) {
+        if (top == stack.length - 1) throw new RuntimeException("Stack Overflow");
+        stack[++top] = x;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T pop() {
+        if (isEmpty()) throw new RuntimeException("Stack Underflow");
+        return (T) stack[top--];
+    }
+
+    @SuppressWarnings("unchecked")
+    public T peek() {
+        if (isEmpty()) throw new RuntimeException("Stack is empty");
+        return (T) stack[top];
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
     }
 }
